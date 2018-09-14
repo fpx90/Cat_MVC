fb93c2dc093f2a204251413114e55fb55744843f
Administrator <Administrator@5IGRINOGV8Q7KL5>
2018-09-14 18:53:52 +0800

    添加项目文件。

---
+++ Cat_SignalR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cat_SignalR.MvcApplication" Language="C#" %>
---
+++ Cat_SignalR/Hubs/CatHub.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Cat_SignalR.Hubs
+{
+    public class CatHub : Hub
+    {
+        private readonly static Dictionary<string, string> connections = new Dictionary<string, string>();
+
+        public static void SendToOne(string username,string content)
+        {
+            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<CatHub>();
+            string userId = "";
+            if (connections.ContainsKey(username))
+            {
+                userId = connections[username];
+                context.Clients.Client(userId).SendMessage(content == "" ? "服务器给你推送消息了" : content);
+            }
+        }
+
+        public void SendLogin(string username)
+        {
+            if (!connections.ContainsKey(username))
+            {
+                connections.Add(username, Context.ConnectionId);
+            }
+            else
+            {
+                connections[username] = Context.ConnectionId;
+            }
+        }
+    }
+}
